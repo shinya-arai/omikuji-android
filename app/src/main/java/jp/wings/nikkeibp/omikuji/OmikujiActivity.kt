092de0182c9a012ec4c8f6d2c85099cef4391865
Arai Shinya <>
2019-05-18 22:53:35 +0900

    first commit

---
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiActivity.kt
@@ -0,0 +1,25 @@
+package jp.wings.nikkeibp.omikuji
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.main.*
+import java.util.*
+
+class OmikujiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+
+        val rnd = Random()
+        val number = rnd.nextInt(20)
+
+        val omikujiShelf = Array<String>(20, {"吉"})
+        omikujiShelf[0] = "大吉"
+        omikujiShelf[19] = "凶"
+
+        var text = omikujiShelf[number]
+
+        hello_view.text = text
+    }
+}
---
+++ app/src/main/java/jp/wings/nikkeibp/omikuji/OmikujiBox.kt
@@ -0,0 +1,14 @@
+package jp.wings.nikkeibp.omikuji
+
+import java.util.*
+
+
+class OmikujiBox {
+    val number: Int
+
+    get() {
+        var rnd = Random()
+        return rnd.nextInt(20)
+    }
+
+}
